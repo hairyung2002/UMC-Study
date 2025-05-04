0698ebeb7fc91409af2b7f19ae950cbc23a16262
hairyung2002 <hairyung2002@naver.com>
2025-05-04 17:58:31 +0900

    FEAT:Miision2

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+const HomePage = () => {
+    return (
+        <div>
+            homepage
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import useForm from "../hooks/useForm";
+import { UserSigninInformation, validateSignin } from "../utils/validate";
+
+const LoginPage = () => {
+
+    const {values, errors, touched, getInputProps} = useForm<UserSigninInformation>({
+        initialValue: {
+            email: "",
+            password: "",
+        },
+        validate: validateSignin,
+    });
+
+    const isDisabled = 
+        Object.values(errors || {}).some((error) => error.length > 0) ||
+        Object.values(values).some((value) => value === "");
+
+
+    const handleSubmit = () => { };
+    return (
+        <div className="flex flex-col items-center justify-center h-full gap-4">
+            <div className="flex flex-col gap-3">
+                <input
+                    {...getInputProps("email")}
+                    name="email"
+                    className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm 
+                        ${errors?.email && touched?.email ? "border-red-500 bg-red-200" : "border-gray-300"}`}
+                    type={"email"}
+                    placeholder={"이메일"}
+                />
+                {errors?.email && touched?.email && (
+                    <div className="text-red-500 text-sm">{errors.email}</div>
+                )}
+                <input
+                    {...getInputProps("password")}
+                    name="password"
+                    className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm 
+                        ${errors?.password && touched?.password ? "border-red-500 bg-red-200" : "border-gray-300"}`}
+                    type={"password"}
+                    placeholder={"패스워드"}
+                />
+                {errors?.password && touched?.password && (
+                    <div className="text-red-500 text-sm">{errors.password}</div>
+                )}
+                <button type="button" onClick={handleSubmit} disabled={isDisabled} className="w-full bg-blue-600 text-white py-3 rounded-md text-lg font-medum hover:bg-blue-700 transition-colors cursor-pointer disabled:bg-gray-300">
+                    로그인
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default LoginPage;
