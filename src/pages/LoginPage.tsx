d881d25abdda81750b577d78cc003051842ade51
hairyung2002 <hairyung2002@naver.com>
2025-05-09 04:02:52 +0900

    FEAT:GoogleLogin

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -22,6 +22,11 @@ const LoginPage = () => {
     const handleSubmit = async() => {
         await login(values);
     };
+
+    const handleGoogleLogin = () => {
+        window.location.href = import.meta.env.VITE_SERVER_API_URL + "/v1/auth/google/login";
+    }
+
     return (
         <div className="flex flex-col items-center justify-center h-full gap-4">
             <div className="flex flex-col gap-3">
@@ -49,6 +54,13 @@ const LoginPage = () => {
                 )}
                 <button type="button" onClick={handleSubmit} disabled={isDisabled} className="w-full bg-blue-600 text-white py-3 rounded-md text-lg font-medum hover:bg-blue-700 transition-colors cursor-pointer disabled:bg-gray-300">
                     로그인
+                </button>
+
+                <button type="button" onClick={handleGoogleLogin} className="w-full bg-blue-600 text-white py-3 rounded-md text-lg font-medum hover:bg-blue-700 transition-colors cursor-pointer disabled:bg-gray-300">
+                    <div className="flex items-center justify-center gap-4">
+                        <img src={"/public/images/GoogleLogo.png"} alt="Google Logo Image" width={'30'}/>
+                        <span>구글 로그인</span>
+                    </div>
                 </button>
             </div>
         </div>
